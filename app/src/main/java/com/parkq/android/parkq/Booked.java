5f8eb234c039c628c53ceef0af5cd5ac47231ce5
RoyalJester <industrialplantlife@yahoo.com>
2016-02-23 19:12:09 -0800

    first commit

--- app/src/main/java/com/parkq/android/parkq/Booked.java
+++ app/src/main/java/com/parkq/android/parkq/Booked.java
@@ -8,25 +8,19 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Booked extends ActionBarActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        TextView mytext = (TextView) findViewById(R.id.sunset_park_id);
-        Typeface mytype = Typeface.createFromAsset(this.getAssets(),"Lato-Black.ttf");
-        mytext.setTypeface(mytype);
+        setContentView(R.layout.activity_booked);
 
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_booked, menu);
         return true;
     }
 
